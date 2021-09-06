344c1c893a3acc41a26176613e5bed413317f99a
Eduardo Garcia <dudufen03@gmail.com>
2021-09-06 10:16:10 -0300

    smooth scroll adicionado

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function expandir() {
     }
     
 }
+window.addEventListener("scroll", function(){
+    var scroll = document.querySelector('.scrollTop');
+    scroll.classList.toggle('active', window.scrollY > 80);
+})
+
+function scrollToTop() {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+}

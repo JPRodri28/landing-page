b5ed83d7a5006528cf7b3dbea83e9a7cada8db3d
Joao-Pedro2804 <joperod2804@gmail.com>
2021-09-08 17:28:08 -0300

    Alteração nas setas do Contato e responsividade de cabeçalho

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function expandir() {
-    var pontos = document.getElementById("pontos");
-    var maisTexto = document.getElementById("mais");
-    var btnseta = document.getElementById("btnseta");
-
-    if (pontos.style.display === "none") {
-        pontos.style.display = "inline";
-        maisTexto.style.display = "none";
-        btnseta.innerHTML = "⬇";
-        
-    }else{
-        pontos.style.display = "none";
-        maisTexto.style.display = "inline";
-        btnseta.innerHTML = "⬆";
-    }
-    
-}
 
 window.addEventListener("scroll", function(){
     var scroll = document.querySelector('.scrollTop');
